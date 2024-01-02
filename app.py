e63d4fefec0776b3ad872c233b76276c0654d783
DONGWEONSHIN <dongweon.shin@gmail.com>
2024-01-02 11:33:58 +0900

    update

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template, request, jsonify
+import markdown
 
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
+# Gemini, LangChain 설정
+import os
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+# PaLM2 설정
+# from langchain.embeddings import VertexAIEmbeddings
+
+# microsoft/DialoGPT-medium 설정
+# tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+# model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+# Gemini 설정
+genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
+model = genai.GenerativeModel(model_name = "gemini-pro")
+
+# LangChain 설정
+llm = ChatGoogleGenerativeAI(model="gemini-pro")
+
+# LangChain 설정 - PaLM2
+# embeddings = VertexAIEmbeddings()
 
 
 app = Flask(__name__)
@@ -20,7 +39,18 @@ def index():
 def chat():
     msg = request.form["msg"]
     input = msg
-    return get_Chat_response(input)
+    
+    # LangChain 설정
+    result = llm.invoke(input)
+    llm_result = result.content
+    
+    
+    # PaLM2 설정
+    # text_embedding = embeddings.embed_query(input)
+    # llm_result = text_embedding
+    
+    return markdown.markdown(llm_result, extensions=['extra'])
+    # return get_Chat_response(input)
 
 
 def get_Chat_response(text):
