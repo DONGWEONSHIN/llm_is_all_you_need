6fb9c90760eb00dcc5c40ad6faf8739cd8ea0ae4
DONGWEONSHIN <dongweon.shin@gmail.com>
2024-01-04 06:23:40 -0500

    update

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.chroma import Chroma
 from langchain.vectorstores.faiss import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.chains import ConversationalRetrievalChain
 
 # pdf 저장폴더
 PDF_DN_FOLDER = "./PDF_DN_FOLDER"
@@ -119,6 +120,11 @@ def chatWithPdf():
     # 파일이 있는지 확인하고,
     # 파일이 없다면 프론트에 fail 리턴
     
+    # 0. 변수 세팅
+    k_value = 4
+    chain_type = "stuff"
+    chat_history = []
+    
     # 1. pdf 가져오기
     loader = PyPDFLoader(fileFullPath)
     pages = loader.load_and_split()
@@ -131,57 +137,38 @@ def chatWithPdf():
     # 3. Gemini 임베딩
     gemini_embedding_model = "models/embedding-001"
     embeddings = GoogleGenerativeAIEmbeddings(model=gemini_embedding_model)
+    # gemini_task_type = "retrieval_document"
+    # embeddings = GoogleGenerativeAIEmbeddings(model=gemini_embedding_model, task_type=gemini_task_type)
     
     # 4. vector store에서 인덱스 생성
-    faiss_index = FAISS.from_documents(pages, embeddings)
+    faiss_index = FAISS.from_documents(docs, embeddings)
     
     # 5. retriever 정의
-    faiss_index.as_retriever(search_type="similarity", search_kwargs={"k": k})
+    retriever = faiss_index.as_retriever(search_type="similarity", search_kwargs={"k": k_value})
+    
+    # 6. chain 설정
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True),
+        retriever=retriever,
+        chain_type=chain_type,
+        return_source_documents=True,
+        return_generated_question=True
+    )
     
     query = msg
-    docs = faiss_index.similarity_search(query)
-    print("##### 2 : query #######", docs[0].page_content)
-    
-    embedding_vector = GoogleGenerativeAIEmbeddings(model=gemini_embedding_model).embed_query(query)
-    docs = faiss_index.similarity_search_by_vector(embedding_vector)
-    print("##### 3 : embedding_vector #######", docs[0].page_content)
+    result = qa({"question": query, "chat_history": chat_history})
     
     
     
+    # docs = faiss_index.similarity_search(query)
+    # print("##### 2 : query #######", docs[0].page_content)
     
+    # embedding_vector = GoogleGenerativeAIEmbeddings(model=gemini_embedding_model).embed_query(query)
+    # docs = faiss_index.similarity_search_by_vector(embedding_vector)
+    # print("##### 3 : embedding_vector #######", docs[0].page_content)
     
+    llm_result = result['answer']
     
-    
-    # 샘플 PDF
-    samlePDF = "langchain.pdf"
-    gemini_embedding_model = "models/embedding-001"
-    gemini_task_type = "retrieval_document"
-    
-    loader = PyPDFLoader(samlePDF)
-    pages = loader.load_and_split()
-    
-    print("##### 1 : page loaded #######", pages[0])
-    
-    embeddings = GoogleGenerativeAIEmbeddings(model=gemini_embedding_model)
-    db = FAISS.from_documents(pages, embeddings)
-    
-    query = request.form["msg"]
-    
-    doc_embeddings = GoogleGenerativeAIEmbeddings(model=gemini_embedding_model, task_type=gemini_task_type)
-    # doc_vecs = [doc_embeddings,embed_query(q) for q in [query, query_2, answer_1]]
-    # docs = db.similarity_search_by_vector(doc_embeddings)
-    # print("##### 3 : doc_embeddings #######", doc_vecs)
-    
-    # return markdown.markdown(docs[0].page_content, extensions=['extra'])
-    
-    
-    
-    
-    # 샘플로 보내 드립니다. 
-    # 나중에 작업 되면 삭제될 내용입니다. 
-    input = msg
-    result = llm.invoke(input)
-    llm_result = result.content
     
     return markdown.markdown(llm_result, extensions=['extra'])
 
