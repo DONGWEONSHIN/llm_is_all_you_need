52590f9e7cdc2c6aeff18c0f62a6ee763c998e78
Jeon0866 <wjs545@naver.com>
2024-01-24 14:00:44 +0900

    code UI UX fix

--- static/chat.js
+++ static/chat.js
@@ -32,6 +32,17 @@ function upload() {
 }
 */
 
+// html button function
+function admin() {
+  window.location.href = '/admin';
+}
+function chatMuseum() {
+  window.location.href = '/chatMuseum';
+}
+function chatDiffusion() {
+  window.location.href = '/chatDiffusion';
+}
+
 $(document).ready(function() {                      // .ready document가 실행되면 한 번만 로드되는 함수
   $("#messageArea").on("submit", function(event) {  // .on id가 messageArea의 form 태그에 submit 이벤트가 발생하면 function() 함수를 실행
       const date = new Date();                      // 날짜
@@ -51,7 +62,7 @@ $(document).ready(function() {                      // .ready document가 실행
               msg: rawText,
           },
           type: "POST",
-          url: "/chatWithPdf",                             //  /chat /chatWithPdf
+          url: "/chat",                             //  /chat /chatWithPdf
       }).done(function(data) {                      // .done 서버로부터 응답이 성공적으로 도착하면 실행되는 함수
           var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://i.ibb.co/fSNP7Rz/icons8-chatgpt-512.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + data + '<span class="msg_time">' + str_time + '</span></div></div>';
           $("#messageFormeight").append($.parseHTML(botHtml));    // #messageFormeight에 botHtml을 추가 
@@ -60,67 +71,95 @@ $(document).ready(function() {                      // .ready document가 실행
     });
     
     //
-    $("#messageAreaMuseum").on("submit", function(event) {  // .on id가 messageArea의 form 태그에 submit 이벤트가 발생하면 function() 함수를 실행
-      const date = new Date();                      // 날짜
-      const hour = date.getHours();                 // 시간
-      const minute = date.getMinutes();             // 분
-      const str_time = hour+":"+minute;             // 시간 : 분
-      var rawText = $("#text").val();               // #text 추출
+    $("#messageAreaPdf").on("submit", function(event) { 
+      const date = new Date();                      
+      const hour = date.getHours();                 
+      const minute = date.getMinutes();             
+      const str_time = hour+":"+minute;             
+      var rawText = $("#text").val();               
 
       var userHtml = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + rawText + '<span class="msg_time_send">'+ str_time + '</span></div><div class="img_cont_msg"><img src="https://i.ibb.co/d5b84Xw/Untitled-design.png" class="rounded-circle user_img_msg"></div></div>';
       
       $("#text").val("");
-      $("#messageFormeight").append(userHtml);      // .append #messageFormeight에 userHTML을 추가
+      $("#messageFormeight").append(userHtml);      
 
 
-      $.ajax({                                      // ajax로 서버와 통신
-          data: {                                   // data안에 msg : rawText를 담는 기능
+      $.ajax({                                      
+          data: {                                   
+              msg: rawText,
+              filename: filename
+          },
+          type: "POST",
+          url: "/chatWithPdf",                             
+      }).done(function(data) {                      
+          var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://i.ibb.co/fSNP7Rz/icons8-chatgpt-512.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + data + '<span class="msg_time">' + str_time + '</span></div></div>';
+          $("#messageFormeight").append($.parseHTML(botHtml));     
+      });                                                         
+      event.preventDefault();                       
+    });
+
+    //
+    $("#messageAreaMuseum").on("submit", function(event) {  
+      const date = new Date();                     
+      const hour = date.getHours();                 
+      const minute = date.getMinutes();             
+      const str_time = hour+":"+minute;             
+      var rawText = $("#text").val();               
+
+      var userHtml = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + rawText + '<span class="msg_time_send">'+ str_time + '</span></div><div class="img_cont_msg"><img src="https://i.ibb.co/d5b84Xw/Untitled-design.png" class="rounded-circle user_img_msg"></div></div>';
+      
+      $("#text").val("");
+      $("#messageFormeight").append(userHtml);      
+
+
+      $.ajax({                                      
+          data: {                                   
               msg: rawText,
               filename:'white_porcelain_square_bottle.pdf'	
           },
           type: "POST",
-          url: "/chatWithPdf",                             //  /chat /chatWithPdf
-      }).done(function(data) {                      // .done 서버로부터 응답이 성공적으로 도착하면 실행되는 함수
+          url: "/chatWithPdf",                           
+      }).done(function(data) {                      
           var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://i.ibb.co/fSNP7Rz/icons8-chatgpt-512.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + data + '<span class="msg_time">' + str_time + '</span></div></div>';
-          $("#messageFormeight").append($.parseHTML(botHtml));    // #messageFormeight에 botHtml을 추가 
-      });                                                         // parseHTML은 문자열로 표현된 HTML을 DOM요소로 변환 -> 변환된 Html 요소를 #messageFormeight에 추가
-      event.preventDefault();                       // form 태그는 기본적으로 페이지 새로고침을 하지만 이것을 방지
+          $("#messageFormeight").append($.parseHTML(botHtml));     
+      });                                                         
+      event.preventDefault();                       
     });
     
     //
-    $("#messageAreaDiffusion").on("submit", function(event) {  // .on id가 messageArea의 form 태그에 submit 이벤트가 발생하면 function() 함수를 실행
-      const date = new Date();                      // 날짜
-      const hour = date.getHours();                 // 시간
-      const minute = date.getMinutes();             // 분
-      const str_time = hour+":"+minute;             // 시간 : 분
-      var rawText = $("#text").val();               // #text 추출
+    $("#messageAreaDiffusion").on("submit", function(event) {  
+      const date = new Date();                      
+      const hour = date.getHours();                 
+      const minute = date.getMinutes();             
+      const str_time = hour+":"+minute;             
+      var rawText = $("#text").val();               
 
       var userHtml = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + rawText + '<span class="msg_time_send">'+ str_time + '</span></div><div class="img_cont_msg"><img src="https://i.ibb.co/d5b84Xw/Untitled-design.png" class="rounded-circle user_img_msg"></div></div>';
       
       $("#text").val("");
-      $("#messageFormeight").append(userHtml);      // .append #messageFormeight에 userHTML을 추가
+      $("#messageFormeight").append(userHtml);      
 
 
-      $.ajax({                                      // ajax로 서버와 통신
-          data: {                                   // data안에 msg : rawText를 담는 기능
+      $.ajax({                                      
+          data: {                                   
               msg: rawText,
               filename:'stable_diffusion_prompt.pdf'	
           },
           type: "POST",
-          url: "/chatWithPdf",                             //  /chat /chatWithPdf
-      }).done(function(data) {                      // .done 서버로부터 응답이 성공적으로 도착하면 실행되는 함수
+          url: "/chatWithPdf",                             
+      }).done(function(data) {                      
           var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://i.ibb.co/fSNP7Rz/icons8-chatgpt-512.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + data + '<span class="msg_time">' + str_time + '</span></div></div>';
-          $("#messageFormeight").append($.parseHTML(botHtml));    // #messageFormeight에 botHtml을 추가 
-      });                                                         // parseHTML은 문자열로 표현된 HTML을 DOM요소로 변환 -> 변환된 Html 요소를 #messageFormeight에 추가
-      event.preventDefault();                       // form 태그는 기본적으로 페이지 새로고침을 하지만 이것을 방지
+          $("#messageFormeight").append($.parseHTML(botHtml));    
+      });                                                         
+      event.preventDefault();                       
     });
 
     //
-    $("#uploadForm").on("submit", function(event) { // uploadForm에 submit이 발생하면 function() 함수를 실행
+    $("#uploadForm").on("submit", function(event) { 
     
       const fileupload = $("#fileupload")[0];
       console.debug();
-        // 파일을 여러개 선택할 수 있으므로 files 라는 객체에 담긴다.
+        
         console.log("fileupload: ", fileupload.files)
     
         if(fileupload.files.length === 0){
@@ -131,7 +170,7 @@ $(document).ready(function() {                      // .ready document가 실행
         const file = new FormData();
         file.append("file", fileupload.files[0]);
         
-        // file 이름 저장
+        
         filename = fileupload.files[0].name;
     
         $.ajax({
@@ -149,4 +188,3 @@ $(document).ready(function() {                      // .ready document가 실행
         });
     });
 });
-
