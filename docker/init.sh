c11b40d84e0aebf6071e993fd9653de99341bff9
DONGWEONSHIN <dongweon.shin@gmail.com>
2024-02-14 14:15:18 +0900

    llama-cpp-python gpu version fixed

--- docker/init.sh
+++ docker/init.sh
@@ -20,7 +20,7 @@ conda install -y pytorch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 pytorch-cu
 conda install -y -c pytorch -c nvidia faiss-gpu=1.7.4 mkl=2021 blas=1.0=mkl
 pip uninstall -y ctransformers llama-cpp-python
 pip install ctransformers[cuda]
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir --verbose
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python==0.2.36 --no-cache-dir --verbose
 
 # Download Llama2 model
 wget https://huggingface.co/LDCC/LDCC-SOLAR-10.7B-GGUF/resolve/main/ggml-model-f16.gguf
